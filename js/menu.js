3ec9a5a36581eb5eea7331d52e22048d6349146f
WanessaMara <wanessamaralima15@gmail.com>
2025-11-23 00:47:17 -0300

    Reconstruindo o portifólio, inserindo menu com navegação. Outras páginas com informações específicas e estilização própria.

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/html/menu.html")
+        .then(response => response.text())
+        .then(html => {
+
+            // Insere o menu carregado
+            document.getElementById("menu").innerHTML = html;
+
+            // ---- MARCAR LINK ATIVO ----
+            const links = document.querySelectorAll("nav a");
+            const path = window.location.pathname;
+            links.forEach(link => {
+                if (link.getAttribute("href") === path) {
+                    link.classList.add("active");
+                }
+            });
+
+            // ---- MENU MOBILE ----
+            const btnMobile = document.getElementById("btn-mobile");
+            const menuMobile = document.getElementById("menu-mobile");
+
+            if (btnMobile && menuMobile) {
+                btnMobile.addEventListener("click", () => {
+                    menuMobile.classList.toggle("show");
+                });
+            }
+
+            // remove fade
+            document.body.classList.add("loaded");
+        });
+});
